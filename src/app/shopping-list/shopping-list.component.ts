3024488b40735dcb31cb55571c1053b303f9ae1e
NRJman <vgrynishyn@gmail.com>
2019-03-16 11:03:01 +0200

    add unsubscription from addIngredient observable

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,23 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Ingredient } from './../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredientsList: Ingredient[];
 
-  constructor(private shoppingListService: ShoppingListService) { }
+  constructor(private shoppingListService: ShoppingListService, private addIngredientSubscription: Subscription) { }
 
   ngOnInit() {
     this.ingredientsList = this.shoppingListService.ingredientsList;
 
-    this.shoppingListService.addIngredient.subscribe((data: { updatedIngredientsList: Ingredient[] }) => {
+    this.addIngredientSubscription = this.shoppingListService.addIngredient.subscribe((data: { updatedIngredientsList: Ingredient[] }) => {
       this.ingredientsList = data.updatedIngredientsList;
     });
+  }
+
+  ngOnDestroy() {
+    this.addIngredientSubscription.unsubscribe();
   }
 
 }
