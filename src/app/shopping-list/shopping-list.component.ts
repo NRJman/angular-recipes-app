d7cd740e6b0fb5636da700e19a81ef1a83f4f50d
NRJman <vgrynishyn@gmail.com>
2019-03-19 18:47:46 +0200

    fix a bug with Subscription provider absence

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Ingredient } from './../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
-import { Subscription } from 'rxjs';
+import { Subscription, SubscriptionLike } from 'rxjs';
 
 @Component({
   selector: 'app-shopping',
@@ -9,9 +9,10 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
+  private addIngredientSubscription: Subscription;
   ingredientsList: Ingredient[];
 
-  constructor(private shoppingListService: ShoppingListService, private addIngredientSubscription: Subscription) { }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
     this.ingredientsList = this.shoppingListService.ingredientsList;
