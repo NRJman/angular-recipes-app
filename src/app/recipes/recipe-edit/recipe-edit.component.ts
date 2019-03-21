4b75fc6733a5f0d2995420aac77add1e6c4805f3
NRJman <vgrynishyn@gmail.com>
2019-03-21 19:31:37 +0200

    synchronize shopping-edit form and recipies data

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Recipe } from '../recipes.model';
 import { ActivatedRoute, Data } from '@angular/router';
 import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
   styleUrls: ['./recipe-edit.component.css']
 })
-export class RecipeEditComponent implements OnInit {
+export class RecipeEditComponent implements OnInit, OnDestroy {
   public recipe: Recipe;
   public recipeForm: FormGroup;
   private editMode: boolean;
   private currentRecipeId: number;
+  private resolverDataSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -28,12 +30,14 @@ export class RecipeEditComponent implements OnInit {
     );
   }
 
-  onDeleteIngredient(id: number) {
+  onDeleteIngredient(id: number): void {
     (<FormArray>this.recipeForm.get('ingredients')).controls.splice(id, 1);
   }
 
+  onSubmitForm(): void {  }
+
   ngOnInit() {
-    this.route.data.subscribe((data: Data) => {
+    this.resolverDataSubscription = this.route.data.subscribe((data: Data) => {
       if (data.recipe.value) {
         this.recipe = data.recipe.value;
         this.currentRecipeId = data.recipe.id;
@@ -49,6 +53,25 @@ export class RecipeEditComponent implements OnInit {
       'description': new FormControl(),
       'ingredients': new FormArray([])
     });
+
+    if (this.editMode) {
+      this.recipeForm.patchValue({
+        'name': this.recipe.recipeName,
+        'imageUrl': this.recipe.recipeImgUrl,
+        'description': this.recipe.recipeDescription
+      });
+
+      this.recipe.recipeIngredients.forEach((ingredient) => {
+        (<FormArray>this.recipeForm.get('ingredients')).push(new FormGroup({
+          'name': new FormControl(ingredient.name),
+          'amount': new FormControl(ingredient.amount)
+        }));
+      });
+    }
+  }
+
+  ngOnDestroy() {
+    this.resolverDataSubscription.unsubscribe();
   }
 
 }
