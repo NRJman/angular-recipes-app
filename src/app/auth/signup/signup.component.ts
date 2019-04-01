40f3651676c3c8117d081b34cef089565ea6becf
NRJman <vgrynishyn@gmail.com>
2019-04-01 21:33:56 +0300

    add missed minlength validator for a password field

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -22,7 +22,7 @@ export class SignupComponent implements OnInit {
   ngOnInit() {
     this.signupForm = new FormGroup({
       'email': new FormControl(null, [Validators.required, Validators.email]),
-      'password': new FormControl(null, Validators.required)
+      'password': new FormControl(null, [Validators.required, Validators.minLength(6)])
     });
   }
 }
